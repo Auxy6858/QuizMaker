0ab89369f04be6e65f060dd990f338e78d9b2555
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2024-01-13 11:21:50 +0000

    Rewrote it to be better

--- QuizMaker.py
+++ QuizMaker.py
@@ -1,153 +1,63 @@
-# IMPORTS
-
 import os
 
+                                        
 
-
-
-
-
-
-
-print("                                                                                ")
-print("                              @@@@@@@@@@@@@@@@@*                                ")
-print("                          @@@@@@@@@@@@@@@@@@@@@@@@@@                            ")
-print("                       @@@@@@@@@@@@&     .@@@@@@@@@@@@                          ")
-print("                     &@@@@@@@@                 %@@@@@@@@                        ")
-print("                    @@@@@@@@     ............     @@@@@@@@                      ")
-print("                   @@@@@@@    .................     @@@@@@@                     ")
-print("                  @@@@@@@    ....................    @@@@@@                     ")
-print("                  @@@@@@&   .....................    @@@@@@@                    ")
-print("                  @@@@@@,   ......................   @@@@@@@                    ")
-print("                  @@@@@@@    ....................    @@@@@@#                    ")
-print("                   @@@@@@@    ..................    @@@@@@@                     ")
-print("                    @@@@@@@     ..............     @@@@@@@                      ")
-print("                     @@@@@@@@.                  @@@@@@@@@                       ")
-print("                       @@@@@@@@@@#          @@@@@@@@@@@                         ")
-print("                         @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@,                      ")
-print("                             @@@@@@@@@@@@@@@@@@@@ @@@@@@@@@@                    ")
-print("                                     (@&*            @@@@@@@@@.                 ")
-print("                                                      @@@@@@@@@@                ")
-print("                                                          @@@@@@@@@             ")
-print("                                                            @@@@@@              ")
-print("")                                                                                
-
-
-
-                                                                                                     
-print("""   .-''-.                                                                                            """)
-print("""  //'` `\|            .--.           __  __   ___                 .           __.....__              """)
-print(""" '/'    '|            |__|          |  |/  `.'   `.             .'|       .-''         '.            """)
-print("""|'      '|            .--.          |   .-.  .-.   '          .'  |      /     .-''"'-.  `. .-..--.  """)
-print("""||     /||            |  |          |  |  |  |  |  |    __   <    |     /     /________\   \|  .-. | """)
-print(""" \'. .'/||     _    _ |  |.--------.|  |  |  |  |  | .:--.'.  |   | ____|                  || |  | | """)
-print("""  `--'` ||    | '  / ||  ||____    ||  |  |  |  |  |/ |   \ | |   | \ .'\    .-------------'| |  | | """)
-print("""        ||   .' | .' ||  |    /   / |  |  |  |  |  |`" __ | | |   |/  .  \    '-.____...---.| |  '-  """)
-print("""        || />/  | /  ||__|  .'   /  |__|  |__|  |__| .'.''| | |    /\  \  `.             .' | |      """)
-print("""        ||//|   `'.  |     /    /___                / /   | |_|   |  \  \   `''-...... -'   | |      """)
-print("""        |'/ '   .'|  '/   |         |               \ \._,\ '/'    \  \  \                  |_|      """)
-print("""        |/   `-'  `--'    |_________|                `--'  `"'------'  '---'                         """)
-print("""                                __                                                                   """)
-print("""                           ...-'  |`.                                                                """)
-print("""                           |      |  |                                                               """)
-print("""                           ....   |  |                                                               """)
-print("""                             -|   |  |                                                               """)
-print("""                              |   |  |                                                               """)
-print("""                           ...'   `--'                                                               """)
-print("""                           |         |`.                                                             """)
-print(""" ,.--.  ,.--.  ,.--.  ,.--.` --------\ |                                                             """)
-print("""//    \//    \//    \//    \`---------'                                                              """)
-print("""\\    /\\    /\\    /\\    /                                                                         """)
-print(""" `'--'  `'--'  `'--'  `'--'                                                                          """)
-                                          
-
-
-
-
-
-fn = input("What would you like the file name to be?   ")
-
-
-qn = input("How many questions do you want to make?  ")
-
+OutputFileName = input("What would you like the file name to be?   ")
+HasGivenQuestionNumber = False
+while HasGivenQuestionNumber == False:
+    TotalQuestions = input("How many questions do you want to make?  ")
+    if TotalQuestions.isnumeric() == True:
+        HasGivenQuestionNumber = True
+        break
+    else:
+        print("Please input a number\n")
+        
 x = 0
 
 
 
 
-outfile = open(fn+'.txt', 'x')
-
+outfile = open(OutputFileName+'.txt', 'x')
 outfile.write('score = 0' + '\n')
-outfile.write('total ='+ qn + '\n')
-
+outfile.write('total = '+ TotalQuestions + '\n')
 outfile.close()
 
-while x < int(qn):
-
-    q = input("What do you want your question to be?   ")
-
-    a = input("What do you want your answer to be? (write in lowercase)  ")
 
 
+while x < int(TotalQuestions):
 
+    IndexNumber = x+1
+    StrIndexNumber = str(IndexNumber)
+    CurrentQuestion = input("What do you want question "+StrIndexNumber+" to be?    ")
+    CurrentAnswer = input("What do you want your answer to be?    ").lower()
+    
+    
 
-    outfile = open(fn+'.txt', "a")
+    outfile = open(OutputFileName+'.txt', "a")
+    
+    
 
-    outfile.write
-    outfile.write(str('ans')), outfile.write(str(x+1)), outfile.write(' = input("'), outfile.write(str(q)), outfile.write(' : ")' + '\n')
-    outfile.write('if ans'), outfile.write(str(x+1)), outfile.write('.lower() =='), outfile.write(str('str("'+ a + '")' + ':'+ '\n')), outfile.write(str('       print("Well done")' + '\n' + '       score = score + 1' + '\n'))
-    outfile.write('else:' + '\n'), outfile.write('       print("Wrong answer!")' + '\n')
+    
+    outfile.write(
+        'Answer'+StrIndexNumber+' = input("'+CurrentQuestion+' : ")' + '\n'+
+        'if Answer'+StrIndexNumber+'.lower() =='+'"'+CurrentAnswer+'"'+':'+'\n'+
+        '       print("Well done")'+'\n'+
+        '       score = score + 1' + '\n'+
+        'else:'+'\n'+
+        '       print("Wrong answer!")'+'\n'
+        )
         
     outfile.close()
-
     x = x + 1
 
-outfile = open(fn+'.txt', 'a')
+ 
 
+outfile = open(OutputFileName+'.txt', 'a')
 outfile.write('print("you scored ",score," out of ",total)' + '\n')
-
 outfile.close()
 
-os.rename(fn+'.txt', fn+'.py')
+os.rename(OutputFileName+'.txt',OutputFileName+'.py')
 
-
-
-
-print("""
-((((((((((((((((((((((((((((((((((((((((((/(/(//////////////////////////////////
-((((((((((((((((((((((((((((((((((((((((((/(/(/(/(/(/(///(//////////////////////
-((((((((((((((((((((((((((((((((((((((/(((//////////////////////////////////////
-((((((((((((((((((((((((((((((((((((((((/(/(/(/(/(///(//////////////////////////
-(((((((((((((((((((((((((((((((((((((///////////////////////////////////////////
-((((((((((((((((((((((((((((((((((((((/(/(/(/(/(/(//////////////////////////////
-((((((((((((((((((((((((((((((((//((////////////////////////////////////////////
-((((((((((((((((((((((((((((((((/(/(/(/(/(/(///(////////////////////////////////
-((((((((((((((((((((((((/((/(///////////////////////////////////////////////////
-((((((((((((((((((((((((((/(((/(/(/(/(/(/(/(////////////////////////////////////
-(((((((((((((((((((/((//////////////////////////////////////////////////////////
-((((((((((((((((((((/(/(/(/(/(/(///(/(//////////////////////////////////////////
-((((((((.                                                               ////////
-((((((((                                                                 ///////
-((((((((                                                                 ///////
-((((((/(          /(/(/.                                                 ///////
-((((((((          /////.                                                 ///////
-((((((((      /(/(/(/(//////      /////   /////        /////   /////,    ///////
-//((////      /////////////                             //*     .//      ///////
-/(((/(/(          /////.                                                 ///////
-////////                                                                 ///////
-/(/(/(/(                                                                 ///////
-//////////                                                             /////////
-/(/(/(//////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-/(//////////////////////                                 ///////////////////////
-////////////////////                                         ///////////////////
-/(/////////////////                                           //////////////////
-//////////      ///                                           ///     ,/////////
-//////////       //                                           //       /////////
-//////////       //                                           //       /////////
-//////////       //                                           //       /////////
-
-""")
 
 print(" Made by https://github.com/Auxy6858 ")
 
