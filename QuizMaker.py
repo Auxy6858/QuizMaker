026114100820ec431d03300158ae0cccf6a0bd97
Auxy6858 <71662994+Auxy6858@users.noreply.github.com>
2024-01-14 18:19:00 +0000

    Cleaned up presentation of QuizMaker.py

--- QuizMaker.py
+++ QuizMaker.py
@@ -1,7 +1,5 @@
 import os
 
-                                        
-
 OutputFileName = input("What would you like the file name to be?   ")
 HasGivenQuestionNumber = False
 while HasGivenQuestionNumber == False:
@@ -14,15 +12,10 @@ while HasGivenQuestionNumber == False:
         
 x = 0
 
-
-
-
 outfile = open(OutputFileName+'.txt', 'x')
 outfile.write('score = 0' + '\n')
 outfile.write('total = '+ TotalQuestions + '\n')
 outfile.close()
-
-
 
 while x < int(TotalQuestions):
 
@@ -30,14 +23,8 @@ while x < int(TotalQuestions):
     StrIndexNumber = str(IndexNumber)
     CurrentQuestion = input("What do you want question "+StrIndexNumber+" to be?    ")
     CurrentAnswer = input("What do you want your answer to be?    ").lower()
-    
-    
-
     outfile = open(OutputFileName+'.txt', "a")
-    
-    
-
-    
+  
     outfile.write(
         'Answer'+StrIndexNumber+' = input("'+CurrentQuestion+' : ")' + '\n'+
         'if Answer'+StrIndexNumber+'.lower() =='+'"'+CurrentAnswer+'"'+':'+'\n'+
@@ -50,17 +37,8 @@ while x < int(TotalQuestions):
     outfile.close()
     x = x + 1
 
- 
-
 outfile = open(OutputFileName+'.txt', 'a')
 outfile.write('print("you scored ",score," out of ",total)' + '\n')
 outfile.close()
-
 os.rename(OutputFileName+'.txt',OutputFileName+'.py')
-
-
 print(" Made by https://github.com/Auxy6858 ")
-
-
-
-
